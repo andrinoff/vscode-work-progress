1d100d4c8db72924418ccab301e882a0147bb6cd
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-04-22 15:14:42 +0400

    Added set to 0 at the end of a clockout and an if statement in case clockout happened before clock in

--- src/time/clockout.ts
+++ src/time/clockout.ts
@@ -4,8 +4,14 @@ import * as fs from "fs";
 import * as vscode from "vscode";
 
 export default function clockOut(context: vscode.ExtensionContext) {
-    console.log("Clock out");
     const start_time = parseInt(context.globalState.get("start_time") || "0");
-    const time_elapsed = JSON.stringify(endTimer(start_time));
-    vscode.window.showInformationMessage(time_elapsed + " minutes you have worked today! \n \n Good job!" );
+    if (start_time === 0) {
+        vscode.window.showErrorMessage("You have not clocked in yet!");
+    }
+    else {
+        console.log("Clock out");
+        const time_elapsed = JSON.stringify(endTimer(start_time));
+        vscode.window.showInformationMessage(time_elapsed + " minutes you have worked today! \n \n Good job!" );
+        context.globalState.update("start_time", "0");
+    }
 }
